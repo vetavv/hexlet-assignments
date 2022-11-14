fb000beed367babf0aebd97fde8a755e6f9aea0b
@hexlet/cli <support@hexlet.io>
2022-11-14 16:21:32 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -1,24 +1,20 @@
 package exercise;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 // BEGIN
 class App {
-    public static boolean scrabble(String strSimbols, String strWord) {
-        var arraySimbols = strSimbols.split("");
-        var arrayWord = strWord.split("");
-        List<String> listSimbols = new ArrayList<>();
-        List<String> listWord = new ArrayList<>();
-        for (String item : arrayWord) {
-            listWord.add(item.toLowerCase());
-        }
-        for (var item : arraySimbols) {
-            listSimbols.add(item.toLowerCase());
-        }
-        for (var item : listWord) {
-            if (listSimbols.contains(item)) {
-                listSimbols.remove(item);
+    public static boolean scrabble(String characterString, String word) {
+        var wordLength = word.length();
+        var charactersArray = characterString.split("");
+        List<String> characters = new ArrayList<>(Arrays.asList(charactersArray));
+
+        for (int i = 0; i < wordLength; i++) {
+            var checkingLetter = word.substring(i, i + 1).toLowerCase();
+            if (characters.contains(checkingLetter)) {
+                characters.remove(checkingLetter);
             } else {
                 return false;
             }
